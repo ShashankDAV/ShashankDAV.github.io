b6caff98d029097e3c3da4d15dbc74a5371389a6
ShashankDAV <112178239+ShashankDAV@users.noreply.github.com>
2022-10-16 13:15:38 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 function run() {
                 var s1= document.getElementById("key").value;
                 var s2= localStorage.getItem(s1)
-                alert(s2)
+                alert(s1)
 }
 
 function fi() {
