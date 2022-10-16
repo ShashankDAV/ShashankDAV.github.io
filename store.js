e28bcd5cd0c063342a72ad2f6ff86c7a4c9dd4b8
ShashankDAV <112178239+ShashankDAV@users.noreply.github.com>
2022-10-16 12:18:31 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,5 +1,9 @@
 function run() {
-         var s1= document.getElementById("key").value;
-         var s2= localStorage.getItem(s1)
-         alert(s2)
+                var s1= document.getElementById("key").value;
+                var s2= localStorage.getItem(s1)
+                alert(s2)
+}
+
+function fi() {
+               alert("hi")
 }
