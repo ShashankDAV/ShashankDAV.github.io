73a89acbbd59759b03e7bd9cefe3672d83737d48
ShashankDAV <112178239+ShashankDAV@users.noreply.github.com>
2022-10-25 14:07:35 +0530

    Delete store.js

--- store.js
+++
@@ -1,9 +0,0 @@
-function run() {
-                var s1= document.getElementById("key").value;
-                var s2= localStorage.getItem(s1)
-                alert(s1)
-}
-
-function fi() {
-               alert("hi")
-}
