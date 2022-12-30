81eed34af68da88c7ae1dc99b4fcaad642b293c8
Shashank Pathak <112178239+ShashankDAV@users.noreply.github.com>
2022-12-30 10:57:48 +0530

    Create con.php

---
+++ trial/con.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "sql6587096";
+$password = "eTghpWCSJ8";
+$db = "sql6587096";
+
+$conn = mysqli($servername, $username, $password, $db);
+
+if($_POST['submit'])
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    
+    $query = "INSERT INTO form VALUES ('$name','$email')";
+    $data = mysqli_query($conn, $query);
+    
+    if($data)
+    {
+       <script>alert("Pass")</script>
+       echo "Successful";
+}
+    else
+    {
+      <script>alert("Fail")</script>
+      echo "Not successful";
+}
+}
+?>
