46cf82c3bb07226fd8fbbed7c2bbcce0ce00a129
Shashank Pathak <112178239+ShashankDAV@users.noreply.github.com>
2023-03-31 17:15:03 +0530

    Update update.py

--- blog/update.py
+++ blog/update.py
@@ -1,7 +1,7 @@
 MDLabel:
-            text : 'Hello , This is updates page'
-            pos_hint : {'center_x':0.5 , 'center_y':0.5}
-            bold : True
-        MDRectangleFlatButton:
-            text : 'Button'
-            pos_hint : {'center_x':0.75 , 'center_y':0.5}
+                text : 'Hello , This is updates page'
+                pos_hint : {'center_x':0.5 , 'center_y':0.5}
+                bold : True
+            MDRectangleFlatButton:
+                text : 'Button'
+                pos_hint : {'center_x':0.75 , 'center_y':0.5}
