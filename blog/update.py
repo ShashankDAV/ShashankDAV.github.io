56ccb5716b8ffe4485f6014c84dfe8b8be331d99
Shashank Pathak <112178239+ShashankDAV@users.noreply.github.com>
2023-04-01 11:21:13 +0530

    Update update.py

--- blog/update.py
+++ blog/update.py
@@ -1,4 +1,16 @@
-MDLabel:
+MDTopAppBar:
+                title : 'ShashankDAV'
+                left_action_items : [['menu' , lambda x = '1' : drawer.set_state('open')]]
+                elevation : -1
+                pos_hint : {'top':1}
+            MDBottomAppBar:
+                MDTopAppBar:
+                    type : 'bottom'
+                    icon : 'home'
+                    elevation : 0
+                   on_action_button : app.root.ids.manager.current = 'home'
+                   pos_hint : {'top':1}
+            MDLabel:
                 text : 'Hello , This is updates page'
                 pos_hint : {'center_x':0.5 , 'center_y':0.5}
                 bold : True
